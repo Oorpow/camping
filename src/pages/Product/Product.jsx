65c30ccb20d0ce3813cc643912682d161007b710
Oorpow <309754513@qq.com>
2022-08-01 17:09:25 +0800

    更新路由处理

---
+++ src/pages/Product/Product.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Outlet } from 'react-router'
+import TentPage from '../TentPage/TentPage'
+
+const Product = () => {
+	return <Outlet />
+}
+
+export default Product
--- src/router/index.js
+++ src/router/index.js
@@ -1,53 +1,79 @@
 import { lazy } from 'react'
+import { Navigate } from 'react-router-dom'
 
 const router = [
 	{
 		path: '/',
-		element: lazy(() => import('../pages/Home/Home')),
-	},
-	{
-		path: '/location',
-		element: lazy(() => import('../pages/Location/Location')),
-		meta: {
-			title: 'Location'
-		}
-	},
-	{
-		path: '/tents',
-		element: lazy(() => import('../pages/Tents/Tents')),
-		meta: {
-			title: 'Tents'
-		}
-	},
-	{
-		path: '/pricing',
-		element: lazy(() => import('../pages/Pricing/Pricing')),
-		meta: {
-			title: 'Pricing'
-		}
-	},
-	{
-		path: '/about',
-		element: lazy(() => import('../pages/About/About')),
-		meta: {
-			title: 'About'
-		}
+		element: lazy(() => import('../components/Layout/Layout')),
+		children: [
+			{
+				index: true,
+				element: <Navigate to='/home' />,
+			},
+			{
+				path: 'product',
+				element: lazy(() => import('../pages/Product/Product')),
+				children: [
+					{
+						path: ':name',
+						element: lazy(() =>
+							import('../pages/TentPage/TentPage')
+						),
+					},
+				],
+			},
+			{
+				path: '/home',
+				element: lazy(() => import('../pages/Home/Home')),
+				meta: {
+					title: 'Home'
+				}
+			},
+			{
+				path: 'location',
+				element: lazy(() => import('../pages/Location/Location')),
+				meta: {
+					title: 'Location',
+				},
+			},
+			{
+				path: 'tents',
+				element: lazy(() => import('../pages/Tents/Tents')),
+				meta: {
+					title: 'Tents',
+				},
+			},
+			{
+				path: 'pricing',
+				element: lazy(() => import('../pages/Pricing/Pricing')),
+				meta: {
+					title: 'Pricing',
+				},
+			},
+			{
+				path: 'about',
+				element: lazy(() => import('../pages/About/About')),
+				meta: {
+					title: 'About',
+				},
+			},
+		],
 	},
 	{
 		path: '/login',
 		element: lazy(() => import('../pages/Login/Login')),
 		meta: {
 			title: 'Sign In',
-			noNeed: true
-		}
+			noNeed: true,
+		},
 	},
 	{
 		path: '/register',
 		element: lazy(() => import('../pages/Register/Register')),
 		meta: {
 			title: 'Sign Up',
-			noNeed: true
-		}
+			noNeed: true,
+		},
 	},
 ]
 
